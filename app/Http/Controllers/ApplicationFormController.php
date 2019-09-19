94465ddec03955f3842d8ec77ad04175bcb1a9ec
prasan <prasan.111it@gmail.com>
2019-09-19 17:01:57 +1000

    send application form email

--- app/Http/Controllers/ApplicationFormController.php
+++ app/Http/Controllers/ApplicationFormController.php
@@ -17,15 +17,11 @@ class ApplicationFormController extends Controller
 
     public function submitApplication(ApplicationFormSubmitRequest $request)
     {
-        try {
-            $pdf = \PDF::loadView('frontend.applicationForm.application_pdf', ['attributes' => $request->all()]);
-            $attributes = $request->all();
-            $attributes['pdf'] = $pdf;
-            Mail::to(config('app.application_to_mail'))->send(new ApplicationFormSubmit($attributes));
-            dd("Mail sent");
-            return redirect()->route('home');
-        }catch (\Exception $e){
-            dd($e);
-        }
+        $pdf = \PDF::loadView('frontend.applicationForm.application_pdf', ['attributes' => $request->all()]);
+        $attributes = $request->all();
+        $attributes['pdf'] = $pdf;
+        Mail::to(config('app.application_to_mail'))->send(new ApplicationFormSubmit($attributes));
+        $request->session()->flash('success', 'Your application has been successfully sent. We will contact you soon!');
+        return redirect()->route('home');
     }
 }
