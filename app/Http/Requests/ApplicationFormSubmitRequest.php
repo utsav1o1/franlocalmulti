5cae052c7271ffcc907559b5e4b58269058684ad
prasan <prasan.111it@gmail.com>
2019-09-19 17:54:42 +1000

    remove validation temporarily

--- app/Http/Requests/ApplicationFormSubmitRequest.php
+++ app/Http/Requests/ApplicationFormSubmitRequest.php
@@ -26,14 +26,6 @@ class ApplicationFormSubmitRequest extends FormRequest
         return [
             'property_street_address' => 'required|max:200',
             'property_suburb' => 'required|max:200',
-            'property_lease_term_years' => 'numeric',
-            'property_lease_term_months' => 'numeric',
-            'property_lease_commencement_date' => 'date',
-            'property_rent' => 'numeric',
-            'property_other_applicants' => 'max:250',
-            'property_occupant_adults' => 'numeric',
-            'property_occupant_child' => 'numeric',
-            'property_children_age' => 'numeric',
 
         ];
     }
