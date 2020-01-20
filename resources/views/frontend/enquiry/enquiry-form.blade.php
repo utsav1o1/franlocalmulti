9385ce9f48ef0308b401f1a795c107bb3a21ca4e
Asis Lohorung <asis.111it@gmail.com>
2020-01-20 16:57:05 +1100

    minor changes 2

--- resources/views/frontend/enquiry/enquiry-form.blade.php
+++ resources/views/frontend/enquiry/enquiry-form.blade.php
@@ -16,6 +16,10 @@
     	<div class="row">
     		<div class="col-md-12">
     			<h1 class="form-title">CONTACT US</h1>
+    			<p>
+    				Isn’t it time you experienced the Multi Dynamic Ingleburn difference? <br>
+    				Here’s how you can get in contact with our team of real estate agents, based throughout Australia.
+    			</p>
     		</div>
     	</div>
     	
