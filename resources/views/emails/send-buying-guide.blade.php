51e6b9b7d764cec1250c81cf1ce16c84679289b3
jay <jthapa.111it@gmail.com>
2021-10-27 10:51:46 +1100

    email content change

--- resources/views/emails/send-buying-guide.blade.php
+++ resources/views/emails/send-buying-guide.blade.php
@@ -1,6 +1,6 @@
-Hi {{$data['name']}},<br />
+Dear {{$data['name']}},<br />
 <br />
-Please find the attachment of buying home guide below:</br />
+Please use the link below to download your Buying Guide copy.</br />
 
 @if(!empty($data['buying']))
 <a href="{!! $data['buying'] !!}" target="_blank">Donwload Buying Guide</a>
--- resources/views/emails/send-selling-guide.blade.php
+++ resources/views/emails/send-selling-guide.blade.php
@@ -1,6 +1,6 @@
-Hi {{$data['name']}},<br />
+Dear {{$data['name']}},<br />
 <br />
-Please find the attachment of selling home guide below:</br />
+Please use the link below to download your Selling Guide copy.</br />
 @if(!empty($data['selling']))
 <a href="{!! $data['selling'] !!}" target="_blank">Donwload Selling Guide</a>
 @endif
