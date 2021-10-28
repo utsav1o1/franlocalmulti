1bdfd339a8c1ae017264087632697d0033f8e276
Dibbya <dibyakarki65@gmail.com>
2021-10-28 20:30:42 +1100

    menu rent commercial fetched agent from corporate

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Auth;
 use App\Mail\AgentInquirySent;
 use App\Repositories\AgentMessageRepository;
 use App\Repositories\AgentRepository;
 use App\Repositories\PropertyRepository;
+use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Contracts\Encryption\DecryptException;
 
 class AgentController extends Controller
 {
@@ -25,8 +24,7 @@ class AgentController extends Controller
         AgentRepository $agents,
         AgentMessageRepository $messages,
         PropertyRepository $properties
-    )
-    {
+    ) {
         $this->agents = $agents;
         $this->messages = $messages;
         $this->properties = $properties;
@@ -38,23 +36,23 @@ class AgentController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {   
+    {
         $existFlag = false;
         $user = [];
         $userProperties = [];
 
-        if(Auth::guard('user')->check()){
+        if (Auth::guard('user')->check()) {
             $user = auth()->guard('user')->user();
             $userProperties = $user->properties()->pluck('id')->toArray();
             $existFlag = in_array($property->id, $userProperties);
         }
 
         return view('agents')->withUser($user)
-                ->withExistFlag($existFlag)
-                ->withRecentProperties(\App\Models\Corporate\Property::where('is_active', '=', 'Y')->where('branch_id', env('BRANCH_ID'))->orderby('created_at', 'desc')->take(5)->get())
-                ->withSavedProperties(\App\Models\Corporate\Property::where('is_active', '=','Y')->where('branch_id', env('BRANCH_ID'))->whereIn('id', $userProperties)->orderby('created_at', 'desc')->take(5)->get())
-                ->withPopularProperties(\App\Models\Corporate\Property::where('is_active', '=','Y')->where('branch_id', env('BRANCH_ID'))->orderby('view_count', 'desc')->take(5)->get())
-                ->withAgents($this->agents->where('is_active','=', '1')->orderby('order_position', 'asc')->paginate(env('PAGINATE')));
+            ->withExistFlag($existFlag)
+            ->withRecentProperties(\App\Models\Corporate\Property::where('is_active', '=', 'Y')->where('branch_id', env('BRANCH_ID'))->orderby('created_at', 'desc')->take(5)->get())
+            ->withSavedProperties(\App\Models\Corporate\Property::where('is_active', '=', 'Y')->where('branch_id', env('BRANCH_ID'))->whereIn('id', $userProperties)->orderby('created_at', 'desc')->take(5)->get())
+            ->withPopularProperties(\App\Models\Corporate\Property::where('is_active', '=', 'Y')->where('branch_id', env('BRANCH_ID'))->orderby('view_count', 'desc')->take(5)->get())
+            ->withAgents($this->agents->where('is_active', '=', 'Y')->orderby('created_at', 'asc')->where('branch_id', env('BRANCH_ID'))->paginate(env('PAGINATE')));
     }
 
     /**
@@ -83,12 +81,12 @@ class AgentController extends Controller
             'full_name' => 'required|min:3|max:255',
             'email_address' => 'required|email',
             'phone_number' => 'required|numeric',
-            'g-recaptcha-response' => 'required|captcha'
+            'g-recaptcha-response' => 'required|captcha',
         ]);
 
         $message = $this->messages->create($request->all());
         if ($message) {
-            if($message->agent){
+            if ($message->agent) {
                 Mail::to($message->agent->email_address)
                     ->send(new AgentInquirySent($message));
             }
