490431084085b59489f196d5f7c5cccc354ada9e
jay <jthapa.111it@gmail.com>
2021-11-02 16:53:46 +1100

    fb pixel added

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -68,11 +68,11 @@ class AgentController extends Controller
         $id = $arr[count($arr) - 1];
         $agent = $this->agents->findByField('id', $id);
         $leased_properties = $agent->properties()->where('is_leased_sold',
-            'Y')->where('property_category_id', 2)->paginate(9);
+            'Y')->where('property_category_id', 2)->paginate(6);
         $sold_properties = $agent->properties()->where('is_leased_sold',
-            'Y')->where('property_category_id', 1)->paginate(9);
-        $rent_properties = $agent->properties()->where('property_category_id', 2)->paginate(9);
-        $buy_properties = $agent->properties()->where('property_category_id', 1)->paginate(9);
+            'Y')->where('property_category_id', 1)->paginate(6);
+        $rent_properties = $agent->properties()->where('property_category_id', 2)->paginate(6);
+        $buy_properties = $agent->properties()->where('property_category_id', 1)->paginate(6);
         // dd($agent->properties()->where('property_category_id', '=', 1)->count());
 
         if ($agent) {
