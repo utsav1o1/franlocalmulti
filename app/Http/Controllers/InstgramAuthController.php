a06ebf2bc142e64b2d0ffb9a91a97efe159b52dc
jay <jthapa.111it@gmail.com>
2022-01-18 16:47:29 +1100

    dd links

--- app/Http/Controllers/InstgramAuthController.php
+++ app/Http/Controllers/InstgramAuthController.php
@@ -7,7 +7,7 @@ class InstgramAuthController extends Controller
     public function show()
     {
         $profile = \Dymantic\InstagramFeed\Profile::where('username', env('INSTAGRAM_USERNAME'))->first();
-        ($profile->getInstagramAuthUrl());
+        dd($profile->getInstagramAuthUrl());
         return view('instagram-auth-page', ['instagram_auth_url' => $profile->getInstagramAuthUrl()]);
     }
 }
