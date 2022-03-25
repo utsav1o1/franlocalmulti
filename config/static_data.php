9c82690f09788b20e6d530c6de1123bb7a743491
subham.shrestha <subham@view9.com.np>
2022-03-25 10:58:01 +0545

    added meta in static data for real estate agents

--- config/static_data.php
+++ config/static_data.php
@@ -3,9 +3,44 @@
 return [
     'real-estate-agent' => [
         'casula'=> [
-            'meta_keywords' => '',
-            'meta_description' => ''
+            'meta_keywords' => 'Casula Real Estate | Multi Dynamic Ingleburn',
+            'meta_description' => 'MD are your local real estate experts in Casula. What can we achieve for you? Contact our team for buying or selling a home or property management.'
         ],
-
+        'liverpool'=> [
+            'meta_keywords' => 'Liverpool Real Estate | Multi Dynamic Ingleburn',
+            'meta_description' => 'MD are your local real estate experts in Liverpool. What can we achieve for you? Contact our team for buying or selling a home or property management.'
+        ],
+        'hoxton-park'=> [
+            'meta_keywords' => 'Hoxton Park Real Estate | Multi Dynamic Ingleburn',
+            'meta_description' => 'MD are your local real estate experts in Hoxton Park. What can we achieve for you? Contact our team for buying or selling a home or property management.'
+        ],
+        'austral'=> [
+            'meta_keywords' => 'Austral Real Estate | Multi Dynamic Ingleburn',
+            'meta_description' => 'MD is your local real estate experts in Austral. What can we achieve for you? Contact our team for buying or selling a home or property management.'
+        ],
+        'bardia'=> [
+            'meta_keywords' => 'Bardia Real Estate | Multi Dynamic Ingleburn',
+            'meta_description' => 'MD are your local real estate experts in Bardia. What can we achieve for you? Contact our team for buying or selling a home or property management.'
+        ],
+        'leppington'=> [
+            'meta_keywords' => 'Leppington Real Estate | Multi Dynamic Ingleburn',
+            'meta_description' => 'MD are your local real estate experts in Leppington. What can we achieve for you? Contact our team for buying or selling a home or property management.'
+        ],
+        'minto'=> [
+            'meta_keywords' => 'Minto Real Estate | Multi Dynamic Ingleburn',
+            'meta_description' => 'MD are your local real estate experts in Minto. What can we achieve for you? Contact our team for buying or selling a home or property management.'
+        ],
+        'glenfield'=> [
+            'meta_keywords' => 'Glenfield Real Estate | Multi Dynamic Ingleburn',
+            'meta_description' => 'MD are your local real estate experts in Glenfield. What can we achieve for you? Contact our team for buying or selling a home or property management.'
+        ],
+        'green-valley'=> [
+            'meta_keywords' => 'Green Valley Real Estate | Multi Dynamic Ingleburn',
+            'meta_description' => 'MD are your local real estate experts in Green Valley. What can we achieve for you? Contact our team for buying or selling a home or property management.'
+        ],
+        'hinchinbrook'=> [
+            'meta_keywords' => 'Hinchinbrook Real Estate | Multi Dynamic Ingleburn',
+            'meta_description' => 'MD are your local real estate experts in Hinchinbrook. What can we achieve for you? Contact our team for buying or selling a home or property management.'
+        ],
     ],
 ];
